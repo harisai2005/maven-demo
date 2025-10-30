e6779d881c060b4f6744b3e6d39aa7676ee3bbbe
Hari Sai Surishetti <harisaisurishetti05@gmail.com>
2025-10-31 01:40:19 +0530

    Main App.java modified

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,22 @@
 package com.example;
 
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI/CD Pipeline!");
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hello from Jenkins Deployed App!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+        server.start();
+        System.out.println("Server started on port 8081");
     }
 }
+
